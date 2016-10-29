374c44208a9009f8a157b2b88c27fe62dde62006
joschaeirich <joscha-friedrich.eirich@stud.uni-bamberg.de>
2016-10-29 18:08:10 +0200

    interface
    this is the interface

---
+++ src/c/main.c
@@ -0,0 +1,23 @@
+#include <pebble.h>
+#include "splash_window.h"
+#include "main_window.h"
+
+
+// Timer to see how long you can see the main window
+void launch_main_window (){
+  window_stack_push(main_window_get_window(), true);
+}
+
+
+int main (){
+
+  splash_window_create();
+  main_window_create();
+  
+  window_stack_push(splash_window_get_window(), true);
+  app_timer_register(3000, launch_main_window, NULL); 
+  
+  
+  app_event_loop();
+  splash_window_destroy();
+}
---
+++ src/c/splash_window.h
@@ -0,0 +1,4 @@
+#pragma once
+void splash_window_create();
+void splash_window_destroy();
+Window *splash_window_get_window();
