03319a366dcfd636409c4bbc2a67fada916eb13e
apfelkraepfla <a.vergere@web.de>
2016-10-30 12:31:08 +0100

    Picture added to sportquestion_window
    https://commons.wikimedia.org/wiki/File:Sports_icon.png#

--- src/c/sportquestion_window.c
+++ src/c/sportquestion_window.c
@@ -7,7 +7,7 @@ static TextLayer *s_label_layer;
 static BitmapLayer *s_icon_layer;
 static ActionBarLayer *s_action_bar_layer;
 
-static GBitmap *s_tick_bitmap, *s_cross_bitmap;
+static GBitmap *s_icon_bitmap, *s_tick_bitmap, *s_cross_bitmap;
 
 
 
@@ -15,10 +15,15 @@ void sportquestion_window_load(Window *window){
  Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
+   s_icon_bitmap = gbitmap_create_with_resource(RESOURCE_ID_CONFIRM);
   
-  
+  const GEdgeInsets icon_insets = {.top = 0, .right = 28, .bottom = 90, .left = 14};
+  s_icon_layer = bitmap_layer_create(grect_inset(bounds, icon_insets));
+  bitmap_layer_set_bitmap(s_icon_layer, s_icon_bitmap);
+  bitmap_layer_set_compositing_mode(s_icon_layer, GCompOpSet);
+layer_add_child(window_layer, bitmap_layer_get_layer(s_icon_layer));
 
-  const GEdgeInsets label_insets = {.top = 10, .right = ACTION_BAR_WIDTH+ACTION_BAR_WIDTH / 2, .left = ACTION_BAR_WIDTH / 2};
+  const GEdgeInsets label_insets = {.top = 60, .right = ACTION_BAR_WIDTH+ACTION_BAR_WIDTH / 2, .left = ACTION_BAR_WIDTH / 2};
   s_label_layer = text_layer_create(grect_inset(bounds, label_insets));
   text_layer_set_text(s_label_layer, SPORTQUESTION_WINDOW_MESSAGE);
   text_layer_set_background_color(s_label_layer, GColorClear);
@@ -41,7 +46,7 @@ void sportquestion_window_unload(Window *window){
   action_bar_layer_destroy(s_action_bar_layer);
   bitmap_layer_destroy(s_icon_layer);
 
-
+  gbitmap_destroy(s_icon_bitmap);
   gbitmap_destroy(s_tick_bitmap);
   gbitmap_destroy(s_cross_bitmap);
 
@@ -52,7 +57,7 @@ sportquestionWindow = NULL;
 void sportquestion_window_create() {
   if(!sportquestionWindow) {
    sportquestionWindow = window_create();
-    window_set_background_color(sportquestionWindow, PBL_IF_COLOR_ELSE(GColorJaegerGreen, GColorWhite));
+    window_set_background_color(sportquestionWindow, PBL_IF_COLOR_ELSE (GColorWhite, GColorWhite));
     window_set_window_handlers(sportquestionWindow, (WindowHandlers) {
         .load = sportquestion_window_load,
         .unload =sportquestion_window_unload,
